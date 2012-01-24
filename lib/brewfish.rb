75dc8db4bf2b0717f5c6842b9a43b14c0ce77eb2
Dylan Paris <dylan.paris+github@gmail.com>
2012-01-24 03:27:03 -0800

    First pass at console module

--- lib/brewfish.rb
+++ lib/brewfish.rb
@@ -1,8 +1,10 @@
-require 'brewfish/version'
+require 'brewfish/console'
 require 'brewfish/point'
 require 'brewfish/line'
 require 'brewfish/color'
 require 'brewfish/util'
+
+require 'brewfish/version'
 
 module Brewfish
   # Your code goes here...
